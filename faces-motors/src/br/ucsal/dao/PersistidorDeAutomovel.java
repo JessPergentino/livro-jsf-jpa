544d6193b4adef5322c411f5a67aa3a1aef92669
Jessica Pergentino <jessicapergentino@hotmail.com>
2018-12-07 10:10:23 -0300

    Criação de uma JPAUtil para o isolamento da criação da EntityManagerFactory - Alteração no persistence.xml para visualização dos comandos sql gerados

--- faces-motors/src/br/ucsal/dao/PersistidorDeAutomovel.java
+++ faces-motors/src/br/ucsal/dao/PersistidorDeAutomovel.java
@@ -1,30 +1,35 @@
 package br.ucsal.dao;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 
 import br.ucsal.model.Automovel;
+import br.ucsal.util.JPAUtil;
 
 public class PersistidorDeAutomovel {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
-		EntityManager em = emf.createEntityManager();
+		
+		// Pegando uma conexão com o banco de dados
+		EntityManager em = JPAUtil.getEntityManager();
 
+		//Criando o Objeto a ser persistido
 		Automovel auto = new Automovel();
-		auto.setAnoFabricacao(2010);
-		auto.setModelo("Ferrari");
+		auto.setAnoFabricacao(2018);
+		auto.setModelo("Spin");
+		auto.setAnoModelo(2018);
+		auto.setMarca("Chevrolet");
 		auto.setObservacoes("Nunca foi batido");
 		
+		//Iniciando a transação
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		em.persist(auto);
 		tx.commit();
+		// Fim da Transação
 		
+		//Fechando a Conexão
 		em.close();
-		emf.close();
 	}
 
 }
